c59bfdc58e1a4d45abad608afdf40e9371745fae
wiris316 <wiris316@gmail.com>
2023-04-05 17:32:54 -0400

    all cards visible

--- components/CreateStudent.jsx
+++ components/CreateStudent.jsx
@@ -1,36 +1,38 @@
-// import React, { useState, useEffect } from 'react';
-// import { Link, withRouter } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link} from 'react-router-dom';
+// import { withRouter } from "react-router";
 
-// // Custom hook for handling input boxes
-// // saves us from creating onChange handlers for them individually
-// const useInput = init => {
-//   const [ value, setValue ] = useState(init);
-//   const onChange = e => {
-//     setValue(e.target.value);
-//   };
-//   // return the value with the onChange function instead of setValue function
-//   return [ value, onChange ];
-// };
+// Custom hook for handling input boxes
+// saves us from creating onChange handlers for them individually
+const useInput = init => {
+  const [ value, setValue ] = useState(init);
+  const onChange = e => {
+    setValue(e.target.value);
+  };
+  // return the value with the onChange function instead of setValue function
+  return [ value, onChange ];
+};
 
-// const CreateStudent = props => {
-//   const [ name, nameOnChange ] = useInput('');
-//   const [ gender, genderOnChange ] = useInput('');
-//   const [ birth_year, birthYearOnChange ] = useInput('');
-//   const [ eye_color, eyeColorOnChange ] = useInput('');
+const CreateStudent = props => {
+  const [ name, nameOnChange ] = useInput('');
+  const [ gender, genderOnChange ] = useInput('');
+  const [ birth_year, birthYearOnChange ] = useInput('');
+  const [ eye_color, eyeColorOnChange ] = useInput('');
 
 
-//   return (
-//     <section className="mainSection createCharContainer">
-//       <header className="pageHeader">
-//         <h2>Add New Student Here</h2>
-//         <Link to="/" className="backLink">
-//           <button type="button" className="btnSecondary">
-//               Back to all characters
-//           </button>
-//         </Link>
-//       </header>
-//     </section>
-//   );
-// };
+  return (
+    <section className="mainSection createCharContainer">
+      <header className="pageHeader">
+        <h2>Add Students to the Roster</h2>
+        <Link to="/" className="backLink">
+          <button type="button" className="btnSecondary">
+              Back to Roster
+          </button>
+        </Link>
+      </header>
 
-// export default withRouter(CreateStudent)
+    </section>
+  );
+};
+
+export default CreateStudent;
