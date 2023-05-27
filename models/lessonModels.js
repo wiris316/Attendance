43609ed59b59e2f7205b0b6d2725a5adb7d57e19
Iris Wong <124114572+wiris316@users.noreply.github.com>
2023-05-27 13:23:10 -0400

    Update lessonModels.js

--- models/lessonModels.js
+++ models/lessonModels.js
@@ -1,6 +1,6 @@
 // // const { Pool } = require('pg');
 
-// // const NPS_URI = 'curl -X GET "https://developer.nps.gov/api/v1/lessonplans?limit=10&start=5&q=lessons" -H  "accept: application/json"'
+// // const NPS_URI = '"'
 
 // // const API_KEY = ""
 // // const pool = new Pool({
